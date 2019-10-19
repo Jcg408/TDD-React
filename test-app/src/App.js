8999d94b0f0a4e948c13f3d165d98ba148b92634
Jessica Gladstone <jessgladtobe@hotmail.com>
2019-10-19 12:53:00 -0400

    initial commit of component and scss

--- test-app/src/App.js
+++ test-app/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Header from './component/header/Header'
+import './app.scss';
 
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Header/>
     </div>
   );
 }
---
+++ test-app/src/component/header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './header.scss';
+
+const Header = (prop) => {
+    return (
+        <header>
+            Header
+        </header>
+    )
+};
+
+export default Header;
